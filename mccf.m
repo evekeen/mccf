884492fee1c43f35ac5a8f55ecd35c90baf3dd37
Alexander Ivkin <a@ivkin.dev>
2021-05-01 10:32:42 -0400

    tracking works

--- mccf.m
+++ mccf.m
@@ -6,20 +6,33 @@ imgs     = dir(fullfile(imgsPath, '*.jpg'));
 
 channels  = 3;
 sigma     = 2;
-lambda    = 1;
+lambda    = 0.1;
 proc_time = 0;
 
 img = imread([imgsPath imgs(1).name]);
-rect = [576.16,336.52,35.39,11.88];
-center = [rect(2)+rect(4)/2 rect(1)+rect(3)/2];
-% img = imcrop(img, rect);
 
-for i = 1:20      
+roi = [576.16,336.52,35.39,11.88];
+w = roi(3);
+h = roi(4);
+WW = 99;
+HH = 99;
+dw = (WW - w) / 2;
+dh = (HH - h) / 2;
+rect = [roi(1) - dw, roi(2) - dh, WW, HH];
+% center = [rect(2)+rect(4)/2 rect(1)+rect(3)/2];
+center = [rect(4)/2 rect(3)/2];
+
+img = imcrop(img, rect);
+im_sz = size(img);
+im_sz = im_sz(1:2);
+cos_window = get_cosine_window(im_sz, 2);
+
+for i = 1:40      
     im = rand_warp(img);
     im = powerNormalise(double(im));
-    im_sz = size(im);
-    im_sz = im_sz(1:2);
+    
     corr_rsp = gaussian_filter(im_sz, sigma, center);
+    im = bsxfun(@times, im, cos_window);
     hogs_f = fft2(im);
     corr_rsp_f = reshape(fft2(corr_rsp), [],1);
     diag_hogs_f = spdiags(reshape(hogs_f, prod(im_sz), []), [0:channels-1]* prod(im_sz), prod(im_sz), prod(im_sz)*(channels));
@@ -49,12 +62,14 @@ for j = 1: channels
     axis off; axis image; title('Orig');
 end
 
-pause;
+% pause;
+prevPos = [rect(1) + WW / 2, rect(2) + HH / 2];
 
 %   testing loop starts here!
 for i = 2:10    
     tic;
     img = imread([imgsPath imgs(i).name]);
+    img = imcrop(img, rect);
     im = powerNormalise(double(img));
     im_sz = size(im);
     im_sz = im_sz(1:2);
@@ -63,7 +78,14 @@ for i = 2:10
     rsp_f  = sum(hogs_f.*filt_f,3);    
     rsp    = circshift(real(ifft2(rsp_f)), -im_sz/2);
     
-    [x y] = find(rsp == max(max(rsp)));    
+    [x y] = find(rsp == max(max(rsp)));
+    newAbs = [rect(1) + x, rect(2) + y];
+    v = newAbs - prevPos;
+    prevPos = newAbs;
+    nextAbs = newAbs + v;    
+    
+    rect = [nextAbs(1) - WW / 2, nextAbs(2) - HH / 2, WW, HH];
+
     proc_time = proc_time + toc;
     
     %   draw ground-truth (blue) and predicited (red) boxes
@@ -76,5 +98,5 @@ for i = 2:10
     hold on; plot(y,x, '*r','MarkerSize',10,'LineWidth',2);
 %     pause(.05);
     pause;        
-end;
+end
 % close aviobj;
--- rand_warp.m
+++ rand_warp.m
@@ -3,6 +3,9 @@ a = -180/16;
 b = 180/16;
 r = a + (b-a).*rand;
 sz = size(img);
-scale = 1-0.1 + 0.2.*rand;
+
+scale = 1-0.6 + 0.7.*rand;
 % trans_scale = randi([-4,4], 1, 1);
 img = imresize(imresize(imrotate(img, r), scale), [sz(1) sz(2)]);
+% imshow(img);
+% pause (0.3);
