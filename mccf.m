d6c2ee564300a0b853bea2d133729d1f3f642260
Alexander Ivkin <a@ivkin.dev>
2021-05-01 13:08:28 -0400

    extracted class

--- mccf.m
+++ mccf.m
@@ -14,8 +14,8 @@ img = imread([imgsPath imgs(1).name]);
 roi = [576.16,336.52,35.39,11.88];
 w = roi(3);
 h = roi(4);
-WW = 119;
-HH = 119;
+WW = 199;
+HH = 199;
 dw = (WW - w) / 2;
 dh = (HH - h) / 2;
 rect = [roi(1) - dw, roi(2) - dh, WW, HH];
@@ -25,33 +25,9 @@ center = [rect(4)/2 rect(3)/2];
 img = imcrop(img, rect);
 im_sz = size(img);
 im_sz = im_sz(1:2);
-cos_window = get_cosine_window(im_sz, 2);
 
-for i = 1:80      
-    im = rand_warp(img);
-    im = powerNormalise(double(im));
-    
-    corr_rsp = gaussian_filter(im_sz, sigma, center);
-%     im = bsxfun(@times, im, cos_window);
-    hogs_f = fft2(im);
-    corr_rsp_f = reshape(fft2(corr_rsp), [],1);
-    diag_hogs_f = spdiags(reshape(hogs_f, prod(im_sz), []), [0:channels-1]* prod(im_sz), prod(im_sz), prod(im_sz)*(channels));
-    if i==1
-        xxF = diag_hogs_f'*diag_hogs_f  ;
-        xyF = diag_hogs_f'*corr_rsp_f ;
-    else
-        xxF = xxF + (diag_hogs_f'*diag_hogs_f);
-        xyF = xyF + (diag_hogs_f'*corr_rsp_f);
-    end
-    
-end
-
-I     = speye(size(xxF,1));
-filtF = (xxF + I*lambda)\xyF;
-filtF = (reshape(filtF, im_sz(1), im_sz(2), []));
-filt  = real(ifft2(filtF));
-filt  = circshift(filt, floor(im_sz/2));
-filt_f = fft2(filt);
+model = Model(im_sz);
+[filt_f, filt] = model.train(img, center, 40);
 
 for j = 1: channels
     subplot(2,3,j) ;imagesc(real(filt(:,:,j)));colormap gray;
@@ -62,7 +38,7 @@ for j = 1: channels
     axis off; axis image; title('Orig');
 end
 
-% pause;
+pause(0.3);
 prevPos = [rect(1) + WW / 2, rect(2) + HH / 2];
 vv = [];
 pos = [];
@@ -83,19 +59,9 @@ for i = 2:40
     rsp    = circshift(real(ifft2(rsp_f)), -im_sz/2);
     
     [y x] = find(rsp == max(max(rsp)));
+    
     % ---- more training --------
-    corr_rsp = gaussian_filter(im_sz, sigma, [x, y]);
-    corr_rsp_f = reshape(fft2(corr_rsp), [],1);
-    diag_hogs_f = spdiags(reshape(hogs_f, prod(im_sz), []), [0:channels-1]* prod(im_sz), prod(im_sz), prod(im_sz)*(channels));    
-    xxF = xxF + (diag_hogs_f'*diag_hogs_f);
-    xyF = xyF + (diag_hogs_f'*corr_rsp_f);
-    I     = speye(size(xxF,1));
-    filtF = (xxF + I*lambda)\xyF;
-    filtF = (reshape(filtF, im_sz(1), im_sz(2), []));
-    filt  = real(ifft2(filtF));
-    filt  = circshift(filt, floor(im_sz/2));
-    filt_f = fft2(filt);
-
+    [filt_f, filt] = model.train(img, [x, y], 1);    
     %----------------------------
     
     newAbs = [rect(1) + x, rect(2) + y];
@@ -126,6 +92,7 @@ for i = 2:40
     hold on; plot(x, y, '*r','MarkerSize',10,'LineWidth',2);
     pause(0.3);
     if i > 2 && diff(1) > 5
+        disp(diff)
         pause
     end
 end
